0d78f33667eee418cd927920390a1963a075ed92
Marcelo Santos <mar27henry@gmail.com>
2020-08-08 21:29:02 -0300

    Init Visual

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+// Importar o CSS Calculator
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render() {
+        return (
+            <div className="calculator">
+                
+            </div>
+        )
+    }
+}
