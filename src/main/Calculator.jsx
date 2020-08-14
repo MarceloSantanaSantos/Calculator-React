f83bb669300aecb7b2460bf5a7f268ffc6576514
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-08-14 14:12:58 -0300

    Teste

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -82,7 +82,7 @@ export default class Calculator extends Component {
         return (
             <div className="calculator">
                 <Display value={this.state.displayValue} />
-                <Button label="AC" click = { this.clearMemory } triple />
+                <Button label="A" click = { this.clearMemory } triple />
                 <Button label="/" click = { this.setOperation } operation/>
                 <Button label="7" click = { this.addDigit} />
                 <Button label="8" click = { this.addDigit} />
